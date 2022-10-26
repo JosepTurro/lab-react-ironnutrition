ae8da7dd2ba6fa114d68c7c2a6c779e71a43683b
Josep Turro <jturrocasanovas@gmail.com>
2022-10-26 17:23:28 +0200

    iteracio dos feta

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+import FoodBox from './components/FoodBox';
+
+
 
 function App() {
+
+   const [foodLst, setFoodLst]= useState(foods)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Food List</h1>
+      {foodLst.map((menjar)=>{
+        
+        return(<div>
+          <p>{menjar.name}</p>
+          <img src= {menjar.image} width={120} />
+        </div>) ;
+      })}
+      <FoodBox food={ {
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      }} />
+      
     </div>
   );
 }
